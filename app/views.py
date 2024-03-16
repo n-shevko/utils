88fa7692f2e23df97d465d762129675d1bb4c115
n-shevko <nikdranik2@gmail.com>
2024-03-15 20:01:42 -0400

    aa

--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 
 from app.utils import get_config
 
 
 def main(request):
     context = {
-        'config': get_config(),
-        'menu': [
-            {'name': 'text_image_feedback_spiral', 'label': 'Text-image feedback spiral'},
-            {'name': 'settings', 'label': 'Settings'}
-        ]
+        'data': {
+            'config': model_to_dict(get_config()),
+            'menu': [
+                {'name': 'text_image_feedback_spiral', 'label': 'Text-image feedback spiral'},
+                {'name': 'settings', 'label': 'Settings'}
+            ]
+        }
     }
     return render(request, "main.html", context)
