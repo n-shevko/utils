9596c3134dd8e03355ff1fc12263b60341825b8c
Antsipau <antsipau@tut.by>
2024-03-23 22:45:35 +0300

    feat: add text_image_feedback_spiral.html, update models, updtate defaults.py, etc

--- app/defaults.py
+++ app/defaults.py
@@ -7,5 +7,16 @@ defaults = {
 
         'script_cleaner_last_out_file': '',
         'script_cleaner_last_answer_gpt': ''
-    }
+    },
+    'text_image_feedback_spiral': {
+        'text_image_feedback_spiral_chat_gpt_max_tokens': '1000',
+        'dall_e_prompt': 'Draw 3 circles',
+        'dall_e_model': 'dall-e-3',
+        'dall_e_size': '1024x1024',
+        'dall_e_quality': 'standard',
+        'dall_e_n': '1',
+        'suggest_changes_prompt': 'What do you think it is better change in this image? Make your suggestions as prompt'
+                                  ' to dalle 3. Provide only suggestions as not numbered list and without '
+                                  'any other text.'
+    },
 }
