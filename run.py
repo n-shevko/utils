7a7dff96651c818da9d1976eeecd83e628d98d87
n-shevko <nikdranik2@gmail.com>
2024-03-24 22:35:40 -0400

    fix

--- run.py
+++ run.py
@@ -8,12 +8,12 @@ def main():
         data = os.path.expanduser('~')
     else:
         data = sys.argv[1]
-    os.system('docker build -t utils_img .')
+    os.system('docker pull nikos123/utils:1.0.0')
     os.system('docker stop -t 0 utils_container')
     os.system('docker rm utils_container')
     user_id = os.getuid()
     group_id = os.getgid()
-    os.system(f"docker run --name utils_container -v service_data:/service_data -v {data}:{data} -e USER_DATA={data} -e USER_ID={user_id} -e GROUP_ID={group_id} -p 8000:8000 utils_img")
+    os.system(f"docker run --name utils_container -v service_data:/service_data -v {data}:{data} -e USER_DATA={data} -e USER_ID={user_id} -e GROUP_ID={group_id} -p 8000:8000 nikos123/utils:1.0.0")
 
 
 if __name__ == '__main__':
