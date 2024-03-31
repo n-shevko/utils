0199fadb94860065ecd818209dffdd8cd5227891
n-shevko <nikdranik2@gmail.com>
2024-03-30 21:29:18 -0400

    fix

--- run.py
+++ run.py
@@ -1,21 +1,21 @@
-import sys
 import os
 import shlex
+import argparse
 
 
 def main():
-    n = len(sys.argv)
-    if n == 1:
-        data = os.path.expanduser('~')
-    else:
-        data = sys.argv[1]
-    data = shlex.quote(data)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, help='Port number (default 8000)', default=8000)
+    parser.add_argument('data', nargs='?', default=os.path.expanduser('~'), help=f"Path to folder that app will see (default {os.path.expanduser('~')})")
+    args = parser.parse_args()
+
+    data = shlex.quote(args.data)
     os.system('docker pull nikos123/utils:1.0.0')
     os.system('docker stop -t 0 utils_container')
     os.system('docker rm utils_container')
     user_id = os.getuid()
     group_id = os.getgid()
-    os.system(f"docker run --name utils_container -v service_data:/service_data -v {data}:{data} -e USER_DATA={data} -e USER_ID={user_id} -e GROUP_ID={group_id} -p 8000:8000 nikos123/utils:1.0.0")
+    os.system(f"docker run --name utils_container -v service_data:/service_data -v {data}:{data} -e USER_DATA={data} -e USER_ID={user_id} -e GROUP_ID={group_id} -p {args.port}:8000 nikos123/utils:1.0.0")
 
 
 if __name__ == '__main__':
