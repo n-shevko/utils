12005472101255f68a6f886fc7bfd582f9159817
n-shevko <nikdranik2@gmail.com>
2024-04-13 12:15:31 -0400

    fix

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.utils.deprecation import MiddlewareMixin
 
@@ -6,7 +8,11 @@ class AutoLoginMiddleware(MiddlewareMixin):
     def process_request(self, request):
         user, created = User.objects.get_or_create(
             username='admin',
-            defaults={'is_staff': True, 'is_superuser': True}
+            defaults={
+                'is_staff': True,
+                'is_superuser': True,
+                'last_login': datetime.datetime.now()
+            }
         )
         user.backend = 'django.contrib.auth.backends.ModelBackend'
         request.user = user
