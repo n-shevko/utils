16a1dc75ecb479aa940ef23c8a5439f76b5c6fe6
n-shevko <nikdranik2@gmail.com>
2024-04-13 12:06:23 -0400

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ mysqld --initialize --datadir=/service_data/mysql
 mysqld --init-file=/src/mysql-init.txt --datadir=/service_data/mysql &
 sleep 2
 python3 manage.py migrate
-chown -R $USER_ID:$GROUP_ID /service_data/mysql
+chown -R $USER_ID:$GROUP_ID /service_data/media
 chmod -R 755 /service_data/media
 exec gosu $USER_ID:$GROUP_ID "$@"
